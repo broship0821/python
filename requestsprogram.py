6e18f488223fd33bcf283b922498844862f775aa
gudqo1995 <gudqo1995@naver.com>
2020-05-27 11:48:12 +0900

    find_all() 한거 [] 리스트에 담아서 출력해보기

--- requestsprogram.py
+++ requestsprogram.py
@@ -5,12 +5,23 @@ req = requests.get(URL)
 html = req.text
 soup = BeautifulSoup(html, 'html.parser')
 list_ = soup.find("ul", class_="type01")
-# 제목 가져오기
+# # 제목 가져오기
+# texts = list_.find_all("dt")
+# for text in texts:
+#     # title = text.find("a", class_="_sp_each_title")
+#     title = text.a["title"]
+# print(title)
+# 제목 가져오기 - 리스트로 묶어서 해보기
 texts = list_.find_all("dt")
-for text in texts:
-    # title = text.find("a", class_="_sp_each_title")
-    title = text.a["title"]
-print(title)
+text_list = [texts]
+print(text_list)
+# for text in text_list:
+#     # title = text.find("a", class_="_sp_each_title")
+#     title = text.a["title"]
+# print(title)
+
+
+
 # 링크 가져오기
 links = list_.find_all("dd", class_="txt_inline")
 for link in links:
